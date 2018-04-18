207a969f62255c2f0ecd861bb9d2484a7e1659eb
EpicEpic46 <gordonreis@gmail.com>
2018-04-18 08:15:35 -0700

    v1.0.0d
    Added a basic paintComponent and DrawImage. Added SOURCEFOLDER final String that *should* work across multiple operating systems

--- Final_Project/graphical/Main.java
+++ Final_Project/graphical/Main.java
@@ -1,16 +1,41 @@
 package graphical;
 
+
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JPanel {
 
+	// begins with file:/ so in order to allow IO to read file, must .substring(6)
+	public final String SOURCEFOLDER = Main.class.getClassLoader().getResource("").toString().substring(6);
+	
 	// methods for different shapes/images
 	
 	protected void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
 		super.paintComponent(g);
-		// insert drawings
+		Image bg;
+		String bgResource = SOURCEFOLDER + "Image.jpg";
+		System.out.println(bgResource);
+		try {
+			bg = ImageIO.read(new File(bgResource));
+		// at 1280*720, 1260*665 is max w*h
+			
+			g.drawImage(bg, 0, 0, 1260, 665, null);
+			
+		} catch (IOException e) {	
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -20,7 +45,7 @@ public class Main extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.add(new Main());
-
+		
 	}
 
 }
